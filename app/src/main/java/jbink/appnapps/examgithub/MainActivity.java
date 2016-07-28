b78ab9b6ad54a2eb14ec02bd3b3f1df63873b213
jbink <jbink17@gmail.com>
2016-07-28 12:28:17 +0900

    Second Commit!!

--- app/src/main/java/jbink/appnapps/examgithub/MainActivity.java
+++ app/src/main/java/jbink/appnapps/examgithub/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Github 추가
+
+        //Second commit
     }
 }
