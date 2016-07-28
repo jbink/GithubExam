18e90767c17159c36a1f8d48fb2f449ec4eab507
jbink <jbink17@gmail.com>
2016-07-28 12:25:54 +0900

    First Commit!!

---
+++ app/src/main/java/jbink/appnapps/examgithub/MainActivity.java
@@ -0,0 +1,14 @@
+package jbink.appnapps.examgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Github 추가
+    }
+}
